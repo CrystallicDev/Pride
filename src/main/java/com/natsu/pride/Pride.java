6b351f3469a8d1dd04348bb5fc3b12a84e28b44d
Nqtsu91 <55384628+Nqtsu91@users.noreply.github.com>
2026-07-16 22:33:19 +0200

    feat: allow joining servers without Pride, all features off

--- src/main/java/com/natsu/pride/Pride.java
+++ src/main/java/com/natsu/pride/Pride.java
@@ -3,9 +3,11 @@ package com.natsu.pride;
 import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import com.natsu.pride.config.ServerConfig;
 
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(Pride.MODID)
 public class Pride {
@@ -13,6 +15,10 @@ public class Pride {
 
     public Pride() {
     	ModLoadingContext.get().registerConfig(Type.SERVER, ServerConfig.SPEC);
+    	// Autorise la connexion aux serveurs sans Pride (vanilla, Paper, Forge sans le mod) :
+    	// toutes les features restent alors désactivées (voir PrideFeature).
+    	ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+    			() -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (remote, isServer) -> true));
     	MixinExtrasBootstrap.init();
     }
 
